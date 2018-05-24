bdbcfef040dcf261b516a7ce03dedc5fdc40a357
Devis <devisbl94@gmail.com>
2018-05-24 14:55:06 -0400

    Adding and setting up redux

---
+++ src/store/reducer.js
@@ -0,0 +1,19 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+    // switch () {
+    //     case value:
+            
+    //         break;
+    
+    //     default:
+    //         break;
+    // }
+}
+
+export default reducer
