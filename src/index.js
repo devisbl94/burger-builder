f2ccace8b8f61f7bfce741d1df850cea81bb8e82
Devis <devisbl94@gmail.com>
2018-05-24 16:08:16 -0400

    Router basename changes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const store = createStore(reducer);
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter basename="/burger-builder">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </Provider>
