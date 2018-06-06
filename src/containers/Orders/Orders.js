696cc68a594b589e48a240d016d7ce3bc88b53f3
Devis <devisbl94@gmail.com>
2018-06-05 20:02:10 -0400

    Adding backend protection by authentication

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -27,7 +27,7 @@ class Orders extends Component {
         //     .catch(err => {
         //         this.setState({ loading: false });
         //     })
-        this.props.onFetchOrders();
+        this.props.onFetchOrders(this.props.token);
     }
     render() {
         let orders = <Spinner />;
@@ -50,13 +50,14 @@ class Orders extends Component {
 const mapStateToProps = state => {
     return {
         orders: state.order.orders,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: () => dispatch(actions.fetchOrders())
+        onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
     }
 }
 
